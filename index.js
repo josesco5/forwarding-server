5e8bb58716f13536ed6df970f35e6df3bec95155
José Escorche <josesco5@gmail.com>
2020-05-10 17:34:12 -0400

    Basic server with Express JS

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const http = require('http');
+import express from 'express'
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
+const port = 8081
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
