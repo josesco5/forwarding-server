cdcb2f9f6bc9b6bfd6dfd475b057c38ed417ac06
José Escorche <josesco5@gmail.com>
2020-05-10 19:06:17 -0400

    Parse JSON and XML requests

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 8081
 
+const jsonParser = bodyParser.json()
+const xmlParser = bodyParser.text({ type: 'application/xml' })
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/rest', jsonParser, (req, res) => {
+  console.log('JSON Params', req.body)
+  res.send({ params: req.body })
+})
+
+app.post('/soap', xmlParser, (req, res) => {
+  console.log('Received XML', req.body)
+  res.set('Content-Type', 'application/xml')
+  res.send(req.body)
+})
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
