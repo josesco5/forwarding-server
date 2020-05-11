30400204beeaeae8c1981d9a10f28fe3f1814572
José Escorche <josesco5@gmail.com>
2020-05-11 11:56:41 -0400

    WIP: Make request to destiny server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import fetch from 'node-fetch'
 
 const app = express()
 const port = 8081
@@ -10,14 +11,32 @@ const xmlParser = bodyParser.text({ type: 'application/xml' })
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/rest', jsonParser, (req, res) => {
+  const destinyUrl = req.get('Destiny-Url')
   console.log('JSON Params', req.body)
-  res.send({ params: req.body })
+  fetch(destinyUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(data)
+    })
 })
 
 app.post('/soap', xmlParser, (req, res) => {
   console.log('Received XML', req.body)
-  res.set('Content-Type', 'application/xml')
-  res.send(req.body)
+  const destinyUrl = req.get('Destiny-Url')
+  const soapAction = req.get('SOAPAction')
+  const headers = {
+    'Content-Type': 'application/xml',
+    'SOAPAction': soapAction
+  }
+  fetch(destinyUrl, { headers, method: 'POST' })
+    .then((response) => {
+      console.log(response)
+      res.set('Content-Type', 'application/xml')
+      res.send(req.body)
+    })
+
+  // res.set('Content-Type', 'application/xml')
+  // res.send(req.body)
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
